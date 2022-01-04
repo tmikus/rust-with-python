5495d148c09c773240360b721ddc0442b48050d9
Tomasz Mikus <tomasz@spiritai.com>
2022-01-04 21:30:11 +0000

    Update the logic to load the python file from disk during the compilation.

--- python_classifier/src/test_classifier.rs
+++ python_classifier/src/test_classifier.rs
@@ -1,15 +1,20 @@
+use std::borrow::Cow;
 use base_classifier::classifier::Classifier;
 use base_classifier::classifier_result::ClassifierResult;
 use pyo3::prelude::*;
 use pyo3::types::{IntoPyDict, PyModule, PyString};
 
+const CLASSIFIER: &'static [u8] = include_bytes!("classifier.py");
+
 pub struct TestClassifier {
+  classifier_code: Cow<'static, str>,
   py_file_path: String,
 }
 
 impl TestClassifier {
   pub fn new(py_file_path: String) -> TestClassifier {
     TestClassifier {
+      classifier_code: String::from_utf8_lossy(CLASSIFIER),
       py_file_path,
     }
   }
@@ -20,11 +25,9 @@ impl Classifier for TestClassifier {
     let result = Python::with_gil(|py| {
       let py_classify: Py<PyAny> = PyModule::from_code(
         py,
-        "def classify(message):
-            return [\"from-python\"]
-        ",
-        "",
-        ""
+        &self.classifier_code,
+        "classifier.py",
+        "python_classifier"
       ).unwrap().getattr("classify").unwrap().into();
       let result = py_classify.call(py, (message,), None).unwrap();
       result.extract::<Vec<String>>(py).unwrap()
